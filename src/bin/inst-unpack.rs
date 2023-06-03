f43021f83633cf6e4053d0d9744c7c44546c4cfa
Raphael Isemann <teemperor@gmail.com>
2023-06-03 15:14:50 +0200

    Add unpack binary

---
+++ src/bin/inst-unpack.rs
@@ -0,0 +1,33 @@
+use std::fs;
+use clap::Parser;
+use crossterm::style::Stylize;
+use riscv_mutator::assembler::assemble_instructions;
+use riscv_mutator::program_input::ProgramInput;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    input: Vec<String>,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    for filename in args.input {
+
+        let buffer = fs::read(filename.clone()).expect("Failed to read file");
+        let input = postcard::from_bytes::<ProgramInput>(buffer.as_slice());
+
+        if input.is_err() {
+            eprintln!(
+                "Note: File not in internal serialized format."
+            );
+            continue;
+        }
+        let program = input.unwrap().insts().to_vec();
+        let bytes = assemble_instructions(&program);
+        let output = filename + ".insts";
+        fs::write(output.clone(), bytes).expect("Unable to write output file");
+        println!("Written output to {}:", output.bold().blue());
+    }
+}
